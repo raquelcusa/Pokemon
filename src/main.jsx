b31619d2670439d57ce7f6dbfc776cdf1f7b7483
Angl0d <136726056+Angl0d@users.noreply.github.com>
2025-11-19 10:11:03 +0100

    Mockup

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Mockup from './PageMockup/Mockup.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <Mockup/>
   </StrictMode>,
 )
