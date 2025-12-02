1c92274b6f217b8854c6c0c277b7b9f2893b4105
gmlluc <lluc.grifol@estudiantat.upc.edu>
2025-12-02 10:58:41 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
-import { useState } from 'react'
+//import { useState } from 'react'
 import './App.css'
 import Header from './components/Header';
 import Footer from './components/Footer';
